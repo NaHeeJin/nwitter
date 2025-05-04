a1db4eb2614b731e7adf6854e9fd351f8353a7b0
NaHeeJin <142037005+NaHeeJin@users.noreply.github.com>
2025-05-04 20:13:56 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB_2QcptvieGCezXe9d9fCRk1wQr9wXI6k",
+  authDomain: "nwitter-da0da.firebaseapp.com",
+  projectId: "nwitter-da0da",
+  storageBucket: "nwitter-da0da.firebasestorage.app",
+  messagingSenderId: "195811127059",
+  appId: "1:195811127059:web:5438ce14110369257f34e6",
+  measurementId: "G-JYQ7HT90GY",
+};
+
+export default firebase.initializeApp(firebaseConfig);
